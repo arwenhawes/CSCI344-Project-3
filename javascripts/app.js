c915920cfedad5ec6a52898ac90c1857805f7cb4
Arwen Hawes <ahawes@unca.edu>
2013-03-21 20:51:12 -0400

    chang app.js to reflect discussion with Semmy

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,82 +1,46 @@
 (function() {
     "use strict";
-    var main,
-        activeTab;
-
+    /*global console: false, document: false, window: false */
+    var $ = window.$,
+            activeTab,
+            createTaskList,
+            main,
+            jsonSetup,
+            allTask,
+            allCategories,
+            removeButton,
+            categoryArray;
+    
+    
+    //changes active tab
+    activeTab = function(anchor) {
+        anchor.click(function() {
+            var target = $(this).attr("href");
+            $(".active").removeClass("active");
+            $(this).addClass("active");
+            $("#" + target).addClass("active");
+            return false;
+        });
+    };
+    jsonSetup = function(todo) {
+        allCategories = $("<ul>" + "<li>" + todo.categories + "</li>").addClass("categoryClass");
+        var arraycategories = [];
+        arraycategories = todo.categories;
+        var joinedArray = arraycategories.join(" ");
+        console.log(joinedArray);
+        allTask = $("<span>" + todo.description + "</span>").addClass(joinedArray);
+        removeButton = $("<button type ='button'>Remove</button>");
+        $("#tasks").append(allTask).append(removeButton).append(allCategories);
+        };
     main = function() {
-        //changes active tab
-        activeTab = function(anchor) {
-            anchor.click(function() {
-                var target = $(this).attr("href");
-                $(".active").removeClass("active");
-                $(this).addClass("active");
-                $("#" + target).addClass("active");
-                return false;
-            });
-        };
-        //builds array that stores categories
-        var categoryArray = [];
-        var createCategoryList = function(category) {
-            if (categoryArray.indexOf(category)===-1){     
-                categoryArray.push(category);
-            };
-            return categoryArray;
-        };
-        
-        //add tasks to category
-        var listCategories = function(categoryArray) {
-            $("#catTab").click(function() {
-              alert("working!"+ categoryArray.length);
-              for (var z = 0; z> categoryArray.length; z++) {
-                   $("#categoryList").append("<ul>" + "<li>" + categoryArray(z) + " </li>");  
-              };
-            });
-        };
-        
-        var addTasksToCategories = function(){
-            $("#categoryList").empty();
-            categoryArray.forEach(function(){
-                $("#categoryList").append("<p>" + category + "</p>");
-                todos.forEach(function(todo){
-                    todo.categories.forEach(function(category){
-                        if (categoryArray.indexOf(category)!==-1){
-                            $("#categoryList").append("<ul>" + "<li>" + description + " </li>");
-                        };
-                    });
-                });
-            });
-        };
-         
-        var buildAddTab = function(){
-          $("#tab3").click(function(){
-              alert("hi there@!");
-              $("addTasks").append("<input type='text' class='new_task' id='nTask' placeholder='task'><br>");
-              $("addTasks").append("<input type='text' class='new_task' id='nCategory' placeholder='categories'><br>");
-              $("addTasks").append("<button class='submit'>Submit</button>");
-              $("addTasks").append("<p>If a task has more than one category, please separate with a comma.</p>");
-              console.log("addTab: Hi There!");
-              });  
-        };
-        
+        //gets all.json data from file
         $.getJSON("all.json", function(todos) {
             todos.forEach(function(todo) {
-                $("#tasks").append("<p>" + todo.description + "<button id='remove'>Remove</button>" + " </p>");
-                todo.categories.forEach(function(category) {
-                   $("#tasks").append("<ul>" + "<li>" + category + " </li>");
-                   createCategoryList(category);                   
-                });       
-            });
-            //tests to make sure array is correct
-            for (var i = 0; i < categoryArray.length; i++) {
-                        console.log(categoryArray[i]);
-                    };
+                jsonSetup(todo);
         });
-        activeTab($(".tabs .tab"));//calls activeTab function
-        //buildCategoryTab(category);
-        buildAddTab();
-        addTasksToCategories();
-        listCategories(categoryArray);
+    });
     };
+        activeTab($(".tabs .tab"));//calls activeTab function
 
     $(document).ready(main);
 }());
