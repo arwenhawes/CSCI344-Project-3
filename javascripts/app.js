2ff9160a4978abe1d8338bcb97e7903122acea96
Arwen Hawes <ahawes@unca.edu>
2013-03-21 23:22:45 -0400

    take lint off code

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,16 +2,16 @@
     "use strict";
     /*global console: false, document: false, window: false */
     var $ = window.$,
-            activeTab,
-            createTaskList,
-            main,
-            jsonSetup,
-            allTask,
-            allCategories,
-            removeButton,
-            categoryArray;
-    
-    
+        activeTab,
+        main,
+        jsonSetup,
+        allTask,
+        allCategories,
+        removeButton,
+        remButton,
+        addTab;
+
+
     //changes active tab
     activeTab = function(anchor) {
         anchor.click(function() {
@@ -24,23 +24,47 @@
     };
     jsonSetup = function(todo) {
         allCategories = $("<ul>" + "<li>" + todo.categories + "</li>").addClass("categoryClass");
-        var arraycategories = [];
+        var joinedArray,
+            arraycategories;
         arraycategories = todo.categories;
-        var joinedArray = arraycategories.join(" ");
+        joinedArray = arraycategories.join(" ");
         console.log(joinedArray);
         allTask = $("<span>" + todo.description + "</span>").addClass(joinedArray);
         removeButton = $("<button type ='button'>Remove</button>");
         $("#tasks").append(allTask).append(removeButton).append(allCategories);
-        };
+    };
+    remButton = function (element) {
+        element.click(function () {
+            $(this).prev().fadeOut(750, function() {
+                $(this).prev().remove();
+            });
+            $(this).nextUntil("span").fadeOut(750, function() {
+                $(this).nextUntil("span").remove();
+            });
+            $(this).fadeOut(5000).remove();
+            return false;
+        });
+    };
+    addTab = function() {
+        var newTask,
+            newCategories = [];
+        newTask = $("#new_task").val();
+        newCategories = $("#new_categories").val();
+        console.log("task: " + newTask + " " + newCategories);
+    };
     main = function() {
         //gets all.json data from file
         $.getJSON("all.json", function(todos) {
             todos.forEach(function(todo) {
                 jsonSetup(todo);
+                remButton(removeButton);
+            });
         });
-    });
+        $(".catTab").click(function() {
+            $(".categoryList").append(".shopping");
+        });
+        $("#submit_button").click(addTab); //calls addTab 
     };
-        activeTab($(".tabs .tab"));//calls activeTab function
-
+    activeTab($(".tabs .tab"));//calls activeTab function
     $(document).ready(main);
 }());
